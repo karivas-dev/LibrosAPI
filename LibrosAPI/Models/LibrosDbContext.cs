416ff0f42916a6d4a157aeef6c6c0f293ba0fb55
karivas-dev <72422927+karivas-dev@users.noreply.github.com>
2024-09-11 18:45:22 -0600

    Initial commit

---
+++ LibrosAPI/Models/LibrosDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibrosAPI.Models
+{
+    public class LibrosDbContext : DbContext
+    {
+        public LibrosDbContext(DbContextOptions<LibrosDbContext> options) : base(options) { }
+        public DbSet<Libro> Libros { get; set; }
+    }
+}
